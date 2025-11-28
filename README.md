ec0381a8e51781f582b82420a6478c098c0a69f5
Cristian Daniel Ramirez Marin <cristiandramirezm.2024@gmail.com>
2025-11-27 19:45:45 -0500

    feature(app): flutter facade project creation

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_facade
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/uranodev/flutter_facade/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.uranodev.flutter_facade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
