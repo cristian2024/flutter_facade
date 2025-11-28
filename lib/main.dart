5cfa59cb68f94f5313c81d9532d47839eee2f401
Cristian Daniel Ramirez Marin <cristiandramirezm.2024@gmail.com>
2025-11-28 07:38:10 -0500

    feature: se crea la pantalla de transferencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facade/ui/screen/transfer_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    final ThemeData themeData = ThemeData.light();
+    return MaterialApp(
+      theme: themeData.copyWith(
+        textTheme: GoogleFonts.plusJakartaSansTextTheme(
+          themeData.textTheme.copyWith(
+            titleLarge: TextStyle(
+              color: themeData.primaryColor,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
         ),
       ),
+      home: TransferScreen(),
     );
   }
 }
